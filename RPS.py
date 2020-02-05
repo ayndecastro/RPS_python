80545343e82c0298d1b9cae3a7e3ff251adc685f
ayndecastro <vincentayndecastro@gmail.com>
2020-02-05 13:50:33 -0600

    added comments

--- RPS.py
+++ RPS.py
@@ -1,10 +1,9 @@
 import random
 
-
 rps = ("Rock", "Paper", "Scissors")
 
 
-class Score:
+class Score:  # score tracking
     def __init__(self):
         self.total = 0
 
@@ -12,11 +11,11 @@ class Score:
         self.total += 1
 
 
-def player_choice():
+def player_choice():  # player input choice
 
     while True:
         x = input("Rock Paper Scissors: ")
-
+        # track the first letter and lowercase it. incase player miss spells or messes up the caps we still get an input
         if x[0].lower() == 'r':
             return "Rock"
         elif x[0].lower() == 'p':
@@ -29,13 +28,18 @@ def player_choice():
         break
 
 
+# use score class to score for both pc and player score tracking
 pc_score = Score()
 player_score = Score()
+
+# we use a while loop to constantly re-initiate rps
 while True:
 
     player = player_choice()
+    # pc choice is chosen in random from the rps tuple using indexing with random module
     pc = rps[random.randrange(0, 3)]
 
+    # game logic
     if player == "Rock" and pc == "Scissors":
         player_score.win()
         print(f'''
@@ -100,22 +104,28 @@ while True:
         PC score: {pc_score.total}
         ''')
 
+    # if pc scores 5 then player loses. It prompts a question if player wants to play again or not.
     if pc_score.total == 5:
         print("YOU LOSE")
         if input("Would you like to play again? yes or no ")[0].lower() == 'y':
+            # if player wants to play again, then we have to reset the score
             player_score.total = 0
             pc_score.total = 0
             continue
         else:
+            # if not, then we break the while loop
             print("Thank you for playing")
             break
 
+    # if player scores 5 then pc loses. It prompts a question if player wants to play again or not.
     if player_score.total == 5:
         print("YOU WIN!")
         if input("Would you like to play again? yes or no ")[0].lower() == 'y':
+            # if player wants to play again, then we have to reset the score
             player_score.total = 0
             pc_score.total = 0
             continue
         else:
+            # if not, then we break the while loop
             print("Thank you for playing")
             break
